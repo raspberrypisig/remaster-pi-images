70317ca0faa8cd94359f0e1dbbab7fad0a63fe53
raspberrypisig <raspberrypisig@gmail.com>
2019-12-06 01:47:41 +0000

    Update shim.sh

--- shim.sh
+++ shim.sh
@@ -4,14 +4,7 @@ set -x
 
 # Run as root
 
-apt update 2>&1 >/dev/null
-set -e
-apt install -y python3-pip
-pip3 install toml
-
-ORIGINAL_IMAGE=$(python3 readConfig.py Release ORIGINAL_IMAGE)
-REMASTERED_IMAGE=$(python3 readConfig.py Release REMASTERED_IMAGE)
-INCREASE_DISK_SIZE_AMOUNT_MB=$(python3 readConfig.py Release INCREASE_DISK_SIZE_AMOUNT_MB)
-ARCH=$(python3 readConfig.py Release ARCH)
-
-bash run-scripts.sh $REMASTERED_IMAGE $ARCH
+touch 2019-09-26-raspbian-buster-lite.img 
+mount --bind ../2019-09-26-raspbian-buster-lite.img 2019-09-26-raspbian-buster-lite.img  
+bash remaster.sh
+umount 2019-09-26-raspbian-buster-lite.img 
